3e227cd05d35430480b912171987b8ea8915a32f
Sio <a@Sios-MacBook-Pro.local>
2022-05-17 20:35:35 -0500

    react-notes-app

---
+++ src/Components/NotesList.js
@@ -0,0 +1,18 @@
+import Note from './Note';
+import AddNote from './AddNote';
+
+const NotesList = ({ notes, handleAddNote, handleDeleteNote, }) => {
+    return (
+    <div className='notes-list'>
+        {notes.map((notes)=> (
+            <Note id={notes.id} 
+            text={notes.text} 
+            date={notes.date}
+            handleDeleteNote={handleDeleteNote}/>
+        ))}
+        <AddNote handleAddNote={handleAddNote}/>
+    </div>
+    );
+};
+
+export default NotesList;
